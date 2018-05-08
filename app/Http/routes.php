39a9963ee51eb1a18c49eb2ace1033146f2c09a4
Ridona <ridonaberisha@gmail.com>
2018-05-08 14:48:58 +0200

    This is a Test Project

---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+
+//Route::get('/user/{id}/post',function($id){
+//    return User::find($id)->post->title;
+//}
+//);
+use App\User;
+use App\Address;
+
+Route::resource('/posts','PostsController');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
